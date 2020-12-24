6ad09c9ad0695b682c72266f7d4c38e8b222b463
Pasit Laothamatas <morp.laothamatas@gmail.com>
2020-12-24 12:38:23 -0800

    wip

--- scripts/travis_ci/print_bin_versions.sh
+++ scripts/travis_ci/print_bin_versions.sh
@@ -8,7 +8,7 @@ source $SCRIPT_DIR/travis_shared.sh
 
 if [ "$RUN_ARM_BUILD" = "true" ] || [ "$RUN_X86_TESTS" = "true" ] || [ "$RUN_CUBE_CODEGEN_CHECKS" = "true" ]; then
   # CMake
-  travis_run cmake --version
+  cmake --version
   # gcc-arm-none-eabi
   travis_run arm-none-eabi-gcc --version
   travis_run arm-none-eabi-objcopy --version
