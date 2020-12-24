acad52359cac3374951140bb343ee83088a581fb
Pasit Laothamatas <morp.laothamatas@gmail.com>
2020-12-24 15:09:42 -0800

    wip

--- scripts/travis_ci/print_bin_versions.sh
+++ scripts/travis_ci/print_bin_versions.sh
@@ -10,7 +10,7 @@ if [ "$RUN_ARM_BUILD" = "true" ] || [ "$RUN_X86_TESTS" = "true" ] || [ "$RUN_CUB
   # CMake
   travis_run cmake --version
   # gcc-arm-none-eabi
-  travis_run arm-none-eabi-gcc --version
+  #travis_run arm-none-eabi-gcc --version
   travis_run arm-none-eabi-objcopy --version
   travis_run arm-none-eabi-objdump --version
   travis_run arm-none-eabi-size --version
