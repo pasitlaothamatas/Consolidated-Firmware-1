aee7ab658d69e23ace4e1b7d7d8bf513d211601e
Pasit Laothamatas <morp.laothamatas@gmail.com>
2020-12-25 01:37:47 -0800

    Wip

--- scripts/travis_ci/print_bin_versions.sh
+++ scripts/travis_ci/print_bin_versions.sh
@@ -10,13 +10,13 @@ if [ "$RUN_ARM_BUILD" = "true" ] || [ "$RUN_X86_TESTS" = "true" ] || [ "$RUN_CUB
   # CMake
   travis_run cmake --version
   # gcc-arm-none-eabi
-  #travis_run arm-none-eabi-gcc --version
+  travis_run arm-none-eabi-gcc --version
   travis_run arm-none-eabi-objcopy --version
   travis_run arm-none-eabi-objdump --version
   travis_run arm-none-eabi-size --version
   travis_run arm-none-eabi-gcc-ar --version
-  #travis_run arm-none-eabi-gdb --version
+  travis_run arm-none-eabi-gdb --version
   # STM32CubeMX doesn't have a --version option unfortunately, we can only
   # check if it is installed correctly
-  #travis_run which STM32CubeMX
+  travis_run which STM32CubeMX
 fi
